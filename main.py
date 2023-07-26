e91bffe7830fbcc22f0ee67e5c6c80d529f6b40e
Minh Nguyen <122658896+minhnguyen220104@users.noreply.github.com>
2023-07-26 10:02:41 +0700

    Update main.py
    Signed-off-by: Minh Nguyen <122658896+minhnguyen220104@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from stravaio import strava_oauth2
 
 app = FastAPI()
 
-@app.get("/hello")
-def hello():
-    return {"Hello"}
+@app.get("/auth")
+def strava_auth():
+    authorization = strava_oauth2(client_id=110799, client_secret='8bac955bd9f61f2ce630f446dc594682afe3901a')
+    return {"Authorization": authorization}
