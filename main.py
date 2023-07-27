548aec16972c54699881a44dd2f97f8460e59299
Minh Nguyen <122658896+minhnguyen220104@users.noreply.github.com>
2023-07-27 07:30:04 +0700

    Update main.py
    Signed-off-by: Minh Nguyen <122658896+minhnguyen220104@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from fastapi import FastAPI
-from stravaio import StravaIO
+from fastapi import FastAPI, HTTPException, Form
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+import json
 
 app = FastAPI()
 
-@app.get("/activities")
-def strava_activities():
-    client = StravaIO(access_token='07691bb1188eca9c01ccdb69f8b1f36a0d596a57')
-    list_activities = client.get_logged_in_athlete_activities()
-    return {"Activities": list_activities}
+# Replace this with your actual reCAPTCHA v3 secret key
+RECAPTCHA_SECRET_KEY = "6Lfv4kwnAAAAAA9PQw7wNkyRo6o4BtP_mT6bj66Q"
+
+@app.post("/verify-recaptcha")
+async def verify_recaptcha(token: str = Form(...)):
+    try:
+        r = requests.post('https://www.google.com/recaptcha/api/siteverify',
+                      data = {'secret' : "6Lfv4kwnAAAAAA9PQw7wNkyRo6o4BtP_mT6bj66Q",
+                              'response' :token})
+        google_response = json.loads(r.text)
+        return  google_response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+origins = ["*"]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*'],
+)
